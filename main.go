223fd481ada9691f950e6461a2f6676175f897cb
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-27 12:18:10 +0800

    Add CORS

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 	"github.com/RichDom2185/go-simple-api-demo/handlers"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
+
 	r.Get("/", handlers.HandleGetHello)
 	r.Post("/", handlers.HandlePostHello)
 
